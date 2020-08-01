c912b19624c85cb66e106db0e42d7d390091a3fd
pkun <181250068@smail.nju.edu.cn>
2020-08-01 12:34:15 +0800

    lazy

--- build_readme.py
+++ build_readme.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
         ]
     )
     readme_contents = readme.open().read()
-    rewritten = replace_chunk(readme_contents, "recent_releases", md)
+    # rewritten = replace_chunk(readme_contents, "recent_releases", md)
 
     # Write out full project-releases.md file
     # project_releases_md = "\n".join(
@@ -168,7 +168,7 @@ if __name__ == "__main__":
 
     code_time_text = "\n```text\n"+fetch_code_time().text+"\n```\n"
 
-    rewritten = replace_chunk(rewritten, "code_time", code_time_text)
+    rewritten = replace_chunk(readme_contents, "code_time", code_time_text)
 
     doubans = fetch_douban()[:5]
 
